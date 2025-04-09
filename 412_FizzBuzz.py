cf84abd25e220b6de0951bf7de7ab3fc3a80a7a6
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2025-04-09 20:05:19 +0530

    solved

--- 412_FizzBuzz.py
+++ 412_FizzBuzz.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         str_list = [str(i) for i in range(1,n+1)]
         for i in str_list:
@@ -9,4 +9,19 @@ class Solution:
                 str_list[i-1] = "Fizz"
             elif i % 5 == 0:
                 str_list[i-1] = "Buzz"
-        return str_list
+            else:
+                
+        return str_list"""
+
+lis=[]
+n=15
+for i in range(1,n+1):
+    if  i % 3 == 0 and i % 5 == 0:
+        lis.append("FizzBuzz")
+    elif i % 3 == 0:
+        lis.append("Fizz")
+    elif i % 5 == 0:
+        lis.append("Buzz")
+    else:
+        lis.append(i)
+print(lis)
