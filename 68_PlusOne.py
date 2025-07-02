ae628b8fc38d2833ae21032da00d21b3bf6d2997
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2025-07-02 12:44:37 +0530

    Update 68_PlusOne.py

--- 68_PlusOne.py
+++ 68_PlusOne.py
@@ -12,7 +12,7 @@ for i in nums:
         nums.append(i)
 print(nums)"""
 
-
+# 1st approach
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         str_s = ""
@@ -22,6 +22,15 @@ class Solution:
         str_s = int(str_s)+1
         for i in str(str_s):
             list1.append(int(i))
-        return list1        
+        return list1  
+
+# 2nd approach
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        str_list = [str(i) for i in digits]
+        joined = "".join(str_list)
+        joined_final = str(int(joined)+1)
+        arr = [int(i) for i in joined_final]
+        return arr
 
 
