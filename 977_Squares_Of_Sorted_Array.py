27999a92e9f36cefc8d974980747af8b949f6850
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2025-07-04 11:54:31 +0530

    Update 977_Squares_Of_Sorted_Array.py : added two pointers method

--- 977_Squares_Of_Sorted_Array.py
+++ 977_Squares_Of_Sorted_Array.py
@@ -1,8 +1,26 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums_squared = []
+        # brute force
+        """nums_squared = []
         for i in nums:
             nums_squared.append(i**2)
-        return sorted(nums_squared)
+        return sorted(nums_squared)"""
+
+        # two pointers
+         left = 0
+        right = len(nums)-1
+        result = []
+        while left<=right:
+            if abs(nums[left]) > abs(nums[right]):
+                result.append(nums[left]**2)
+                left+=1
+            else:
+                result.append(nums[right]**2)
+                right-=1
+        result.reverse()
+        return result
+        
+
+        
 
         
