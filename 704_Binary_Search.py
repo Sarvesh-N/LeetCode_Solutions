ebd0a6f411e7e707bf5dab8b9f6df84a415cde6e
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2025-08-25 11:41:17 +0530

    Update 704_Binary_Search.py

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,18 +1,17 @@
-def binary_search(arr:list, target:int)->int:
-    start, end = 0, len(arr)-1
-    
-    while start <= end:
-        mid = (start+end)//2
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        nums.sort()
 
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            end = mid+1
-        elif arr[mid] > target:
-            start = mid-1 
-    return -1
+        while start <= end:
+            mid  = (start+end)//2
 
-arr = [3, 5, 1, 10, 20, 10, 12, 5]
-arr.sort()
-tar = 1
-print(binary_search(arr, tar))
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid -1
+            elif nums[mid] < target:
+                start = mid+1
+        return -1
+
+        
