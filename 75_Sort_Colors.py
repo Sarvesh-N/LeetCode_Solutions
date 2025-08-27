47822557cbd80ee543f4852fdc13b3a2ee440dec
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2025-08-27 23:25:14 +0530

    Create 75_Sort_Colors.py

---
+++ 75_Sort_Colors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        # for i in range(n):
+        #     for j in range(n-i-1):
+        #         if nums[j] > nums[j+1]:
+        #             nums[j], nums[j+1] = nums[j+1], nums[j]
+        # return nums
+
+        for i in range(n):
+            mini = i
+            for j in range(i+1, n):
+                if nums[j] < nums[mini]:
+                    mini = j
+            nums[i], nums[mini] = nums[mini], nums[i]
+        return nums
+
+
+
+        
