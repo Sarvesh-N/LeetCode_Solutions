93f25007089e60512b92696b07ebd77d2726b9d2
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2025-11-07 23:01:12 +0530

    Create 1484_Group_Sold_Products_By_The_Date.sql

---
+++ 1484_Group_Sold_Products_By_The_Date.sql
@@ -0,0 +1,10 @@
+with cte as (
+    select sell_date,
+    count(distinct product) as num_sold,
+    group_concat(distinct product order by product separator ',') as products
+    from Activities 
+    group by sell_date
+)
+
+select * from cte
+order by sell_date
