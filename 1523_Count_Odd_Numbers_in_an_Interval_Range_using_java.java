84a9c9433875745bed1cef4365d045265b18e6b7
sarvesh <145215133+Sarvesh-N@users.noreply.github.com>
2026-02-25 20:10:59 +0530

    Create 1523_Count_Odd_Numbers_in_an_Interval_Range_using_java.java

---
+++ 1523_Count_Odd_Numbers_in_an_Interval_Range_using_java.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int res = 0;
+        if (low % 2 == 0){
+            low++;
+        }
+        for(int i = low; i<= high; i+=2){
+            res++;
+        }
+        return res;
+    }
+}
